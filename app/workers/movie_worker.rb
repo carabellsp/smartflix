7ede6cb3500aa66619a28d8d47a593d14045f233
Cara Bell <cara.bell@192.168.1.108>
2021-06-21 17:35:17 +0100

    add comments to worker

--- app/workers/movie_worker.rb
+++ app/workers/movie_worker.rb
@@ -7,16 +7,19 @@ class MovieWorker
     def perform
       title = Faker::Movie.title
       return if Movie.find_by(title: title)
+      # because we are using Faker, we do not want to generate multiple instances of the same movie
 
       response = HTTParty.get("http://www.omdbapi.com/?apikey=#{ENV['OMDB_API_KEY']}&t=#{title}")
-  
+
       movie = Movie.create!(title: response['Title'], year: response['Year'], released: response['Released'],
                             genre: response['Genre'], director: response['Director'], plot: response['Plot'],
                             language: response['Language'], runtime: response['Runtime'])
 
       response['Actors'].split(', ').each do |actor_name|
         ActiveRecord::Base.transaction do
+          # wrapped the create methods in a transaction to ensure it rolls back if not fully completing
           actor = Actor.find_or_create_by(full_name: actor_name) do |actor|
+            # use this find_or_create_by method to avoid duplicate actor creation
             actor.first_name = actor_name.split[0]
             actor.last_name = actor_name.split[-1]
           end
