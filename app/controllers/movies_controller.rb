7c6aa5dc61434353e7bcf367ae8ad58331651e70
Cara Bell <cara.bell@192.168.1.109>
2021-06-22 12:15:28 +0100

    refactor movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,14 +3,22 @@ class MoviesController < ApplicationController
   def show
     title = params[:title]
 
-    @movie = Movie.find_by(title: title)
+    @movie = find_movie_by_title(title)
 
     if @movie
       render json: @movie
     else
-      MovieWorker.perform_async(title)
+      add_movie(title)
       render json: {:error => "We do not yet have this movie :("}.to_json,
              status: 404
     end
+  end
+
+  def find_movie_by_title(title)
+    Movie.find_by(title: title)
+  end
+
+  def add_movie(title)
+    MovieWorker.perform_async(title)
   end
 end
