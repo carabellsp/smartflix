a09cc5623c21c28f5079c76939d565bfb796f7f7
Cara Bell <cara.bell@192.168.1.106>
2021-06-29 15:28:10 +0100

    add actor model spec

---
+++ spec/models/actor_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Actor, :type => :model do
+
+  subject { described_class.new(full_name: 'Cara Bell',
+                                first_name: 'Cara',
+                                last_name: 'Bell')
+          }
+
+  # let(:actor_with_values) { described_class.new(full_name: 'Cara Bell', first_name: 'Cara', last_name: 'Bell') }
+  # let(:actor_without_values) { described_class.new(full_name: nil) }
+
+  it 'is valid with valid attributes' do
+    expect(subject).to be_valid
+  end
+
+  it 'has the correct attributes' do
+    expect(subject).to have_attributes(full_name: 'Cara Bell')
+  end
+
+  it 'is not valid without a full name' do
+    subject.full_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a first name' do
+    subject.first_name = nil
+    expect(subject).to_not be_valid
+  end
+
+  it 'is not valid without a last name' do
+    subject.last_name = nil
+    expect(subject).to_not be_valid
+  end
+end
+
