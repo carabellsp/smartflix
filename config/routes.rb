573c5ebf8866cf9bd4bcbf26d302d28f10c05cad
Cara Bell <cara.bell@192.168.1.106>
2021-07-05 15:07:30 +0100

    update config for factory bot

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ require 'sidekiq/web'
 Rails.application.routes.draw do
   mount Sidekiq::Web => '/sidekiq' # show the sidekiq web UI at localhost/sidekiq
   get '/movies/:title' => 'movies#show'
+
+  resources :movies
 end
