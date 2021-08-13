1dacc0422c96690d7e82f401c00f28d5a299ef39
Cara Bell <cara.bell@192.168.1.176>
2021-08-13 15:17:50 +0100

    update method name and extract variable

--- app/api/omdb/api_adapter.rb
+++ app/api/omdb/api_adapter.rb
@@ -6,10 +6,11 @@ require 'httparty'
 # Service to parse response from OMDB Api
 module Omdb
   class ApiAdapter
-    def fetch_data(title = nil)
+    def fetch_response(title = nil)
       title = movie_title if title.nil?
       base_uri = "http://www.omdbapi.com/?apikey=#{ENV['OMDB_API_KEY']}"
-      response = HTTParty.get("#{base_uri}&t=#{title}")
+      uri = "#{base_uri}&t=#{title}"
+      response = HTTParty.get(uri)
 
       if response.body.include?('False')
         Rails.logger.warn "This movie '#{title}' requested at #{Time.current} has returned an error in the response"
