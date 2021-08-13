5b910d33aaa0cda07223d8e204fc6d98804e9596
Cara Bell <cara.bell@192.168.1.176>
2021-08-13 15:18:23 +0100

    add spec for api adapter

--- spec/api/omdb/api_adapter_spec.rb
+++ spec/api/omdb/api_adapter_spec.rb
@@ -1,1 +1,18 @@
+require 'rails_helper'
 
+RSpec.describe Omdb::ApiAdapter do
+  describe '#fetch_response' do
+
+    context 'when title is provided' do
+      let(:title) { 'Home Alone' }
+
+      it 'makes a request and returns the correct HTTP response' do
+        VCR.use_cassette 'fetch_home_alone' do
+          response = subject.fetch_response(title)
+          expect(response).to be_a_kind_of HTTParty::Response
+          expect(response[:title]).to eq('Home Alone')
+        end
+      end
+    end
+  end
+end
