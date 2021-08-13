1e10eaae75883c439ca471fe52359154abb2691e
Cara Bell <cara.bell@192.168.1.176>
2021-08-13 15:09:25 +0100

    annotate vcr config

--- spec/support/vcr_config.rb
+++ spec/support/vcr_config.rb
@@ -3,8 +3,8 @@
 require 'vcr'
 
 VCR.configure do |config|
-  config.cassette_library_dir = 'spec/vcr_cassettes'
+  config.cassette_library_dir = 'spec/vcr_cassettes' # the directory where VCR will store its cassettes (HTTP interactions)
   config.configure_rspec_metadata!
-  config.hook_into :webmock
-  config.ignore_localhost = true
+  config.hook_into :webmock # connects vcr w webmock to stub requests
+  config.ignore_localhost = true # ensures VCR ignores requests to localhost
 end
