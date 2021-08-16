7806769ff174bddba5904f7a93acde375be48fed
Cara Bell <cara.bell@192.168.1.202>
2021-08-16 12:46:32 +0100

    add frozen string literal comment

--- app/units/create_movie/action.rb
+++ app/units/create_movie/action.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CreateMovie
   class Action
     def call(response)
--- app/units/create_movie/entry_point.rb
+++ app/units/create_movie/entry_point.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CreateMovie
   class EntryPoint
     def initialize(response)
