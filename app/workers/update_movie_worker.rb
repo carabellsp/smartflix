944a497ad581eb4c0a1251ffb4d9727dacc513d5
Cara Bell <cara.bell@192.168.1.202>
2021-08-17 15:26:34 +0100

    implement new update movie worker

---
+++ app/workers/update_movie_worker.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'sidekiq-scheduler'
+
+# Sidekiq Worker to Update Movie Instances (scheduled everyday at 7am)
+class UpdateMovieWorker
+  include Sidekiq::Worker
+
+  sidekiq_options retry: false, queue: 'movies'
+
+  def perform
+    movies = Movie.all.select(:title)
+    movies.each do |movie|
+      response = omdb_adapter.fetch_response(movie.title)
+      UpdateMovie::EntryPoint.new(response, movie)
+    end
+  end
+
+  private
+
+  def omdb_adapter
+    @omdb_adapter ||= Omdb::ApiAdapter.new
+  end
+end
