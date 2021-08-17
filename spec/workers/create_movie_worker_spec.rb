7584531f5a16c837aad9d809a10a12b4f6bc7003
Cara Bell <cara.bell@192.168.1.202>
2021-08-17 16:27:22 +0100

    minor change to create movie worker spec

--- spec/workers/create_movie_worker_spec.rb
+++ spec/workers/create_movie_worker_spec.rb
@@ -4,14 +4,14 @@ require 'rails_helper'
 require 'sidekiq/testing'
 
 RSpec.describe CreateMovieWorker, :vcr do
-  subject { described_class.new.perform(movie) }
+  subject { described_class.new }
 
   let(:movie) { 'Black Panther' }
 
   Sidekiq::Testing.inline!
 
   it 'adds a movie', :vcr, :aggregate_failures do
-    expect { subject }.to change(Movie, :count).by(1)
+    expect { subject.perform(movie) }.to change(Movie, :count).by(1)
   end
 
   # it 'sets the correct attributes' do
