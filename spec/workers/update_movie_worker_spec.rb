43017b681d1fe2b8b8ba3b68256d288f795fdf13
Cara Bell <cara.bell@192.168.1.202>
2021-08-17 16:13:40 +0100

    add specs for update_movie worker

---
+++ spec/workers/update_movie_worker_spec.rb
@@ -0,0 +1,40 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe UpdateMovieWorker do
+  subject { described_class.new }
+
+  let!(:movie) { create(:movie, title: 'The Notebook') }
+  let(:dummy_omdb_adapter) { instance_double(Omdb::ApiAdapter) }
+  let(:response) { instance_double(HTTParty::Response, body: response_body) }
+  let(:response_body) do
+    { 'Title' => 'The Notebook',
+      'Year' => 2004,
+      'Released' => 'Fri, 25 Jun 2004',
+      'Genre' => 'Drama, Romance',
+      'Director' => 'Nick Cassavetes',
+      'Actors' => 'Rachel McAdams, Ryan Gosling',
+      'Plot' => 'A poor yet passionate young man falls in love with a rich young woman, giving her a        sense of freedom, but they are soon separated because of their social differences',
+      'Language' => 'English',
+      'Runtime' => 123 }
+  end
+
+  before do
+    allow(subject).to receive(:omdb_adapter).and_return(dummy_omdb_adapter)
+    allow(dummy_omdb_adapter).to receive(:fetch_response).and_return(response)
+    allow(response).to receive(:parsed_response).and_return(response_body)
+  end
+
+  it 'updates the movie record' do
+    subject.perform
+    expect(movie.reload).to have_attributes(runtime: 123, year: 2004, language: 'English', genre: 'Drama, Romance')
+  end
+
+  it 'calls the UpdateMovie::EntryPoint' do
+    expect(UpdateMovie::EntryPoint).to receive(:new)
+
+    subject.perform
+  end
+end
+
