5f1a90724a3aef8f06bcdddb1616db6c69d9bd69
Cara Bell <cara.bell@192.168.1.202>
2021-08-17 16:39:51 +0100

    add destroy_movie specs and shared_examples

---
+++ spec/units/destroy_movie/entry_point_spec.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+require_relative 'destroy_movie_shared_examples'
+
+RSpec.describe DestroyMovie::EntryPoint do
+  subject { described_class.new(movie) }
+
+  let!(:movie) { create(:movie) }
+
+  include_examples 'successfully deletes movie object'
+
+end
