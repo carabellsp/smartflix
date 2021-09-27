83400ad07e4e42ac49f607fbee9ff99831c561e3
Cara Bell <cara.bell@MLH-0612.local>
2021-09-27 13:50:22 +0100

    remove full_name validation for actor

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Actor < ApplicationRecord
-  validates :full_name, presence: true
   validates :first_name, presence: true
   validates :last_name, presence: true
 
